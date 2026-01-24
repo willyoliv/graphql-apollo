6e919f9b387d9d853b2f652b1cfd9ada747a3a5e
Willy Oliveira <willyoliversilva@gmail.com>
2026-01-24 17:02:52 -0300

    feat(index.js): created graphql server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-console.log('hello world!');
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello from Apollo 4!',
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: 4003 },
+}).then(({ url }) => {
+  console.log(`🚀 Server ready at: ${url}`);
+});
