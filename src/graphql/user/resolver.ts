84b5c8a098547223854bf3c99c683b60a1603b62
Willy Oliveira <willyoliversilva@gmail.com>
2026-02-16 12:52:51 -0300

    styles: changed resolver type

--- src/graphql/user/resolver.ts
+++ src/graphql/user/resolver.ts
@@ -1,22 +1,20 @@
 import { cleanUrlFilterParams } from '../../utils/cleanUrlFilterParams';
-import type { Resolvers } from '../../generated/graphql';
+import type { QueryResolvers, Resolvers } from '../../generated/graphql';
 
-type QueryTypes = Required<Resolvers>['Query'];
-
-const users: QueryTypes['users'] = async (_, { filters }, { getUsers }) => {
+const users: QueryResolvers['users'] = async (_, { filters }, { getUsers }) => {
   const cleanFilters = cleanUrlFilterParams(filters);
   const apiFiltersInput = new URLSearchParams(cleanFilters);
   const users = await getUsers('/?' + apiFiltersInput);
   return users.json();
 };
 
-const user: QueryTypes['user'] = async (_, { id }, { getUsers }) => {
+const user: QueryResolvers['user'] = async (_, { id }, { getUsers }) => {
   const response = await getUsers('/' + id);
   const user = await response.json();
   return user;
 };
 
-export const userResolvers: QueryTypes = {
+export const userResolvers: Resolvers = {
   Query: {
     user,
     users,
